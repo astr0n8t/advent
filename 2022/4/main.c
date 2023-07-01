ad639634e0db906bb915d9af28ae03939350da80
Nathan Higley <contact@nathanhigley.com>
2023-06-30 19:30:35 -0500

    feat: finish part 2

--- 2022/4/main.c
+++ 2022/4/main.c
@@ -9,9 +9,11 @@ struct schedule {
 	int stop2;
 };
 
+// Function to return max of two ints
 int max(int a, int b) {
 	return (a >= b) ? a : b; 
 }
+// Function to return min of two ints
 int min(int a, int b) {
 	return (a <= b) ? a : b; 
 }
@@ -27,24 +29,35 @@ int main(int argc, char *argv[])
 
 	// Declare stack vars
 	struct schedule current;
-	int total = 0;
+	int contained_total = 0;
+	int overlapping_total = 0;
 	
-	//
 	// Iterate over the file, grabbing the pairs
 	while(fscanf(in_file, "%d-%d,%d-%d\n", &current.start1, &current.stop1, 
 				&current.start2, &current.stop2) == 4) {
+
+		// Check for contained ranges
 		if ((min(current.start1, current.start2) == current.start1 
 			&& max(current.stop1, current.stop2) == current.stop1) ||
 			(min(current.start2, current.start1) == current.start2 
 			&& max(current.stop2, current.stop1) == current.stop2)) {
-			printf("%d-%d,%d-%d\n", current.start1, current.stop1, 
-				current.start2, current.stop2);
-			total = total + 1;
+
+			contained_total = contained_total + 1;
+		}
+
+		// Check for overlapping ranges
+		if ((max(current.stop1, current.start2) == current.stop1
+			&& min(current.start1, current.start2) == current.start1) ||
+			(max(current.stop2, current.start1) == current.stop2
+			&& min(current.start2, current.start1) == current.start2)) {
+
+			overlapping_total = overlapping_total + 1;
 		}
 	}
 
 	// Output the total
-	printf("The total number contained in is %d\n", total);
+	printf("The total number contained in is %d\n", contained_total);
+	printf("The total number overlapping is %d\n", overlapping_total);
 
 	fclose(in_file);
 	return EXIT_SUCCESS;
