5eb8b94d06da3faf6d3788c65a40b6979e9e73d9
Nathan Higley <contact@nathanhigley.com>
2023-08-05 17:35:53 -0500

    feat: cleanup day 18

--- 2022/18/main.c
+++ 2022/18/main.c
@@ -2,13 +2,20 @@
 #include <stdio.h>
 #include <string.h>
 
-// Basic Point struct
+
+// Matter definitions
+#define AIR 0
+#define CUBE 1
+#define WATER 2
+
+// Cube struct to hold 3d coordinates
 struct Cube {
 	int x;
 	int y;
 	int z;
 };
 
+// 3d array List-like struct
 struct Space {
 	int*** array;
 	int width;
@@ -16,119 +23,85 @@ struct Space {
 	int depth;
 };
 
+// List of cubes
 struct CubeList {
 	struct Cube* array;
 	size_t size;
 };
 
-void fillcracks(struct Space environment, struct Cube c) {
-	struct Cube tmp = c;
-	if (environment.array[c.x+1][c.y][c.z] == 0) {
-		tmp.x = c.x+1;
-		environment.array[c.x+1][c.y][c.z] = 2;
-		fillcracks(environment, tmp);
-	}
-	if (environment.array[c.x-1][c.y][c.z] == 0) {
-		tmp = c;
-		tmp.x = c.x-1;
-		environment.array[c.x-1][c.y][c.z] = 2;
-		fillcracks(environment, tmp);
-	}
-	if (environment.array[c.x][c.y+1][c.z] == 0) {
-		tmp = c;
-		tmp.y = c.y+1;
-		environment.array[c.x][c.y+1][c.z] = 2;
-		fillcracks(environment, tmp);
-	}
-	if (environment.array[c.x][c.y-1][c.z] == 0) {
-		tmp = c;
-		tmp.y = c.y-1;
-		environment.array[c.x][c.y-1][c.z] = 2;
-		fillcracks(environment, tmp);
-	}
-	if (environment.array[c.x][c.y][c.z+1] == 0) {
-		tmp = c;
-		tmp.z = c.z+1;
-		environment.array[c.x][c.y][c.z+1] = 2;
-		fillcracks(environment, tmp);
-	}
-	if (environment.array[c.x][c.y][c.z-1] == 0) {
-		tmp = c;
-		tmp.z = c.z-1;
-		environment.array[c.x][c.y][c.z-1] = 2;
+// We need a function definition since these functions call
+// each other
+void fillcracks(struct Space environment, struct Cube c);
+
+// Function to fill a spot with water and continue to fill cracks
+void fill(struct Space environment, int x, int y, int z) {
+	struct Cube tmp = {.x=x, .y=y, .z=z};
+	if (environment.array[x][y][z] == AIR) {
+		environment.array[x][y][z] = WATER;
 		fillcracks(environment, tmp);
 	}
 	return;
 }
 
 
+// Brute force recursive function to find all air pockets
+// that aren't visible from the surface but are still exposed
+void fillcracks(struct Space environment, struct Cube c) {
+	// Check the x+1 case
+	fill(environment, c.x+1, c.y, c.z);
+	fill(environment, c.x-1, c.y, c.z);
+	fill(environment, c.x, c.y+1, c.z);
+	fill(environment, c.x, c.y-1, c.z);
+	fill(environment, c.x, c.y, c.z+1);
+	fill(environment, c.x, c.y, c.z-1);
+	return;
+}
+
+
+// Brute force function to fill all outside air with water
 void waterfill(struct Space environment) {
 	struct Cube tmp;
 
+	// Evaluate from front and back
 	for (int x=0; x<environment.width; x++) {
 		for (int y=0; y<environment.height; y++) {
-			for (int z=0; z<environment.depth; z++) {
-				if (environment.array[x][y][z]==1) {
-					break;
-				}
-				else {
-					environment.array[x][y][z] = 2;
-				}
+			for (int z=0; z<environment.depth && environment.array[x][y][z] != CUBE; z++) {
+				environment.array[x][y][z] = WATER;
 			}
-			for (int z=environment.depth-1; z>-1; z--) {
-				if (environment.array[x][y][z]==1) {
-					break;
-				}
-				else {
-					environment.array[x][y][z] = 2;
-				}
+			for (int z=environment.depth-1; z>-1 && environment.array[x][y][z] != CUBE; z--) {
+				environment.array[x][y][z] = WATER;
 			}
 		}
 	}
+	// Evaluate from left and right
 	for (int y=0; y<environment.height; y++) {
 		for (int z=0; z<environment.depth; z++) {
-			for (int x=0; x<environment.width; x++) {
-				if (environment.array[x][y][z]==1) {
-					break;
-				}
-				else {
-					environment.array[x][y][z] = 2;
-				}
+			for (int x=0; x<environment.width && environment.array[x][y][z] != CUBE; x++) {
+				environment.array[x][y][z] = WATER;
 			}
-			for (int x=environment.width-1; x>-1; x--) {
-				if (environment.array[x][y][z]==1) {
-					break;
-				}
-				else {
-					environment.array[x][y][z] = 2;
-				}
+			for (int x=environment.width-1; x>-1 && environment.array[x][y][z] != CUBE; x--) {
+				environment.array[x][y][z] = WATER;
 			}
 		}
 	}
+	// Evaulate from top and bottom
 	for (int z=0; z<environment.depth; z++) {
 		for (int x=0; x<environment.width; x++) {
-			for (int y=0; y<environment.height; y++) {
-				if (environment.array[x][y][z]==1) {
-					break;
-				}
-				else {
-					environment.array[x][y][z] = 2;
-				}
+			for (int y=0; y<environment.height && environment.array[x][y][z] != CUBE; y++) {
+				environment.array[x][y][z] = WATER;
 			}
-			for (int y=environment.height-1; y>-1; y--) {
-				if (environment.array[x][y][z]==1) {
-					break;
-				}
-				else {
-					environment.array[x][y][z] = 2;
-				}
+			for (int y=environment.height-1; y>-1 && environment.array[x][y][z] != CUBE; y--) {
+				environment.array[x][y][z] = 2;
 			}
 		}
 	}
 
+	// Find all cracks previously unfound
 	for (int x=1; x<environment.width-1; x++) {
 		for (int y=1; y<environment.height-1; y++) {
 			for (int z=1; z<environment.depth-1; z++) {
+				// If this is water we need to check if there is air around it
+				// so call fillcracks on every water cube
 				if (environment.array[x][y][z] == 2) {
 					tmp.x = x;
 					tmp.y = y;
@@ -143,8 +116,11 @@ void waterfill(struct Space environment) {
 
 // Solve part1
 int part1(struct Space environment, struct CubeList list) {
+	// stack vars
 	int result = 0;
 	for (int i=0; i<list.size; i++) {
+		// Quick calculation basically CUBEs are equal to 1 and AIR is equal to 0
+		// water is not involved yet
 		result = result + 6 - 
 			environment.array[list.array[i].x+1][list.array[i].y][list.array[i].z] -
 			environment.array[list.array[i].x-1][list.array[i].y][list.array[i].z] -
@@ -158,26 +134,28 @@ int part1(struct Space environment, struct CubeList list) {
 
 // Solve part2
 int part2(struct Space environment, struct CubeList list) {
+	// Fill the environment exterior with water
 	waterfill(environment);
+	// Stack vars
 	int result = 0;
-	int collision_check = 0;
 	for (int i=0; i<list.size; i++) {
-		if(environment.array[list.array[i].x+1][list.array[i].y][list.array[i].z]==2) {
+		// Check every block to the left, right, up, down, back, front and see if it is water
+		if(environment.array[list.array[i].x+1][list.array[i].y][list.array[i].z]==WATER) {
 			result++;
 		}
-		if(environment.array[list.array[i].x-1][list.array[i].y][list.array[i].z]==2) {
+		if(environment.array[list.array[i].x-1][list.array[i].y][list.array[i].z]==WATER) {
 			result++;
 		}
-		if(environment.array[list.array[i].x][list.array[i].y+1][list.array[i].z]==2) {
+		if(environment.array[list.array[i].x][list.array[i].y+1][list.array[i].z]==WATER) {
 			result++;
 		}
-		if(environment.array[list.array[i].x][list.array[i].y-1][list.array[i].z]==2) {
+		if(environment.array[list.array[i].x][list.array[i].y-1][list.array[i].z]==WATER) {
 			result++;
 		}
-		if(environment.array[list.array[i].x][list.array[i].y][list.array[i].z+1]==2) {
+		if(environment.array[list.array[i].x][list.array[i].y][list.array[i].z+1]==WATER) {
 			result++;
 		}
-		if(environment.array[list.array[i].x][list.array[i].y][list.array[i].z-1]==2) {
+		if(environment.array[list.array[i].x][list.array[i].y][list.array[i].z-1]==WATER) {
 			result++;
 		}
 	}
@@ -187,23 +165,28 @@ int part2(struct Space environment, struct CubeList list) {
 // Function to parse the input file and return
 // a string representing the wind
 struct Space processinput(FILE *in_file, struct CubeList* list) {
+	// Stack vars
 	struct Space output = {.height=0, .width=0, .depth=0};
 	char buffer[100];
 	int curr = 0;
 
+	// Get the total number of cubers
 	while(fgets(buffer, sizeof(buffer), in_file) != NULL) {
 		// Check if the line is blank
 		if (buffer[0] != '\n') {
 			list->size++;
 		}
 	}
+	// Go back to beginning of file
 	rewind(in_file);
 
+	// Allocate our list memory
 	list->array = (struct Cube*)malloc(sizeof(struct Cube)*list->size);
 
 	// Read in our input file
 	while(fscanf(in_file, "%d,%d,%d\n", &list->array[curr].x, &list->array[curr].y, 
 				&list->array[curr].z) == 3) {
+		// Check for maxes
 		if (list->array[curr].x > output.width) {
 			output.width = list->array[curr].x;
 		}
@@ -215,11 +198,12 @@ struct Space processinput(FILE *in_file, struct CubeList* list) {
 		}
 		curr++;
 	}
+	// We need to add 2 to each dimension so we can safely check dimension +1 and -1
 	output.width = output.width + 2;
 	output.height = output.height + 2;
 	output.depth = output.depth + 2;
 
-	// Allocate our space array
+	// Allocate our 3d space array
 	output.array = (int***)malloc(sizeof(int*)*output.width + 
 			sizeof(int**)*output.width*output.height + 
 			sizeof(int)*output.width*output.height*output.depth);
@@ -240,11 +224,10 @@ struct Space processinput(FILE *in_file, struct CubeList* list) {
 
 	// Set all of our cube locations
 	for (int i=0; i<list->size; i++) {
-		//printf("Setting %d %d %d\n", list->array[i].x, list->array[i].y, list->array[i].z);
-		output.array[list->array[i].x][list->array[i].y][list->array[i].z] = 1;
+		output.array[list->array[i].x][list->array[i].y][list->array[i].z] = CUBE;
 	}
 
-	// Return the wind array
+	// Return the space
 	return output;
 }
 
@@ -257,7 +240,7 @@ int main(int argc, char *argv[])
 		exit(-1);
 	}
 	// Declare stack vars
-	// Get our wind from the input file
+	// Get our list and environment from the input file
 	struct CubeList list = {.size=0};
 	struct Space environment = processinput(in_file, &list);
 	// Solve part 1
