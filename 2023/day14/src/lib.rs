004a5105014c044a036649f16c61dd2d3e979eb1
Nathan Higley <contact@nathanhigley.com>
2023-12-14 17:57:44 +0000

    fix: optimization for day 14

--- 2023/day14/src/lib.rs
+++ 2023/day14/src/lib.rs
@@ -1,3 +1,7 @@
+#![feature(test)]
+extern crate test;
+use test::Bencher;
+
 use std::fs;
 use std::collections::HashMap;
 use std::collections::hash_map::DefaultHasher;
@@ -5,7 +9,7 @@ use std::hash::{Hash, Hasher};
 
 pub fn part1(input_file: &str) -> usize {
     let mut input = parse_input(input_file);
-    cycle(&mut input, 0);
+    north(&mut input);
     load(&input)
 }
 
@@ -13,12 +17,11 @@ pub fn part2(input_file: &str) -> usize {
     let mut input = parse_input(input_file);
     let mut history: HashMap<u64,usize> = HashMap::new();
     let mut num: usize = 0;
-    while !history.contains_key(&calculate_hash(&input)) {
-        history.insert(calculate_hash(&input), num);
-        cycle(&mut input,0);
-        cycle(&mut input,1);
-        cycle(&mut input,2);
-        cycle(&mut input,3);
+    let mut curr = calculate_hash(&input);
+    while !history.contains_key(&curr) {
+        history.insert(curr, num);
+        cycle(&mut input);
+        curr = calculate_hash(&input);
         num += 1;
     }
 
@@ -28,10 +31,7 @@ pub fn part2(input_file: &str) -> usize {
     let num_to_go = (period - num_before_repeat) + (1000000000 % period);
 
     for _ in 0..num_to_go {
-        cycle(&mut input,0);
-        cycle(&mut input,1);
-        cycle(&mut input,2);
-        cycle(&mut input,3);
+        cycle(&mut input);
     }
 
     load(&input)
@@ -59,77 +59,66 @@ fn load(rocks: &Vec<Vec<char>>) -> usize {
     sum
 }
 
-fn cycle(rocks: &mut Vec<Vec<char>>, dir: u8) {
-    match dir {
-        0 => {
-            for x in 0..rocks[0].len() {
-                for mut y in 0..rocks.len() {
-                    match rocks[y][x] {
-                        'O' => {
-                            while y != 0 && rocks[y-1][x] == '.' {
-                                rocks[y-1][x] = rocks[y][x];
-                                rocks[y][x] = '.';
-                                y -= 1;
-                            }
-                        },
-                        _ => (),
-                    }
+fn cycle(rocks: &mut Vec<Vec<char>>) {
+    north(rocks);
+    west(rocks);
+    south(rocks);
+    east(rocks);
+}
+fn north(rocks: &mut Vec<Vec<char>>) {
+    for x in 0..rocks[0].len() {
+        for mut y in 0..rocks.len() {
+            if rocks[y][x] == 'O' {
+                while y != 0 && rocks[y-1][x] == '.' {
+                    rocks[y-1][x] = rocks[y][x];
+                    rocks[y][x] = '.';
+                    y -= 1;
 
                 }
             }
-        },
-        1 => {
-            for y in 0..rocks.len() {
-                for mut x in 0..rocks[0].len() {
-                    match rocks[y][x] {
-                        'O' => {
-                            while x != 0 && rocks[y][x-1] == '.' {
-                                rocks[y][x-1] = rocks[y][x];
-                                rocks[y][x] = '.';
-                                x -= 1;
-                            }
-                        },
-                        _ => (),
-                    }
-
+        }
+    }
+}
+fn west(rocks: &mut Vec<Vec<char>>) {
+    for y in 0..rocks.len() {
+        for mut x in 0..rocks[0].len() {
+            if rocks[y][x] == 'O' {
+                while x != 0 && rocks[y][x-1] == '.' {
+                    rocks[y][x-1] = rocks[y][x];
+                    rocks[y][x] = '.';
+                    x -= 1;
                 }
             }
-        },
-        2 => {
-            for x in 0..rocks[0].len() {
-                for mut y in (0..rocks.len()).rev() {
-                    match rocks[y][x] {
-                        'O' => {
-                            while y != rocks.len()-1 && rocks[y+1][x] == '.' {
-                                rocks[y+1][x] = rocks[y][x];
-                                rocks[y][x] = '.';
-                                y += 1;
-                            }
-                        },
-                        _ => (),
-                    }
 
+        }
+    }
+}
+fn south(rocks: &mut Vec<Vec<char>>) {
+    for x in 0..rocks[0].len() {
+        for mut y in (0..rocks.len()).rev() {
+            if rocks[y][x] == 'O' {
+                while y != rocks.len()-1 && rocks[y+1][x] == '.' {
+                    rocks[y+1][x] = rocks[y][x];
+                    rocks[y][x] = '.';
+                    y += 1;
                 }
             }
-        },
-        3 => {
-            for y in 0..rocks.len() {
-                for mut x in (0..rocks[0].len()).rev() {
-                    match rocks[y][x] {
-                        'O' => {
-                            while x != rocks[0].len()-1 && rocks[y][x+1] == '.' {
-                                rocks[y][x+1] = rocks[y][x];
-                                rocks[y][x] = '.';
-                                x += 1;
-                            }
-                        },
-                        _ => (),
-                    }
 
+        }
+    }
+}
+fn east(rocks: &mut Vec<Vec<char>>) {
+    for y in 0..rocks.len() {
+        for mut x in (0..rocks[0].len()).rev() {
+            if rocks[y][x] == 'O' {
+                while x != rocks[0].len()-1 && rocks[y][x+1] == '.' {
+                    rocks[y][x+1] = rocks[y][x];
+                    rocks[y][x] = '.';
+                    x += 1;
                 }
             }
-        },
-        _ => ()
+
+        }
     }
 }
 
@@ -140,6 +129,19 @@ fn parse_input(input_file: &str) -> Vec<Vec<char>> {
     input.trim().split("\n")
         .map(|x| x.chars().collect() )
         .collect::<Vec<Vec<char>>>()
+}
+
+#[bench]
+fn bench_part1(b: &mut Bencher) {
+    b.iter(|| {
+        part1("data/input.txt");
+    });
+}
+#[bench]
+fn bench_part2(b: &mut Bencher) {
+    b.iter(|| {
+        part2("data/input.txt");
+    });
 }
 
 #[cfg(test)]
