29dd0ae47e85f387a8d8e7b077481bc2ba11105a
Nathan Higley <contact@nathanhigley.com>
2023-12-21 17:42:56 +0000

    feat: solve day 21 part 2

--- 2023/day21/src/lib.rs
+++ 2023/day21/src/lib.rs
@@ -4,11 +4,11 @@ use std::collections::VecDeque;
 use std::collections::HashSet;
 
 pub fn part1(input_file: &str) -> usize {
-    parse_input(input_file).get_num_plots(64)
+    parse_input(input_file).get_num_plots(64, false)
 }
 
 pub fn part2(input_file: &str) -> usize {
-    0
+    parse_input(input_file).estimate_num_plots(26501365)
 }
 
 #[derive(Copy,Clone,Debug,PartialEq)]
@@ -40,6 +40,17 @@ impl Position {
     fn west(&self) -> Self {
         Self{ x: self.x - 1, y: self.y }
     }
+    fn modulo(&self, x_mod: usize, y_mod: usize) -> Self {
+        let mut x = self.x % x_mod as isize;
+        let mut y = self.y % y_mod as isize;
+        if x < 0 {
+            x += x_mod as isize;
+        }
+        if y < 0 {
+            y += y_mod as isize;
+        }
+        Self { x: x, y: y }
+    }
 }
 
 #[derive(Debug)]
@@ -51,7 +62,21 @@ struct Maze {
 struct MazeError;
 
 impl Maze {
-    fn get_num_plots(&self, max: usize) -> usize {
+    fn estimate_num_plots(&self, num: usize) -> usize {
+        let modulo = self.maze[0].len();
+        let rem = num % modulo;
+        let y1 = self.get_num_plots(rem, true) as isize;
+        let y2 = self.get_num_plots(rem+modulo, true) as isize;
+        let y3 = self.get_num_plots(rem+modulo*2, true) as isize;
+
+        let x_2 = (y1 - 2*y2 + y3) as f64 / 2.0;
+        let x_1 = (4*y2 - y3 - 3*y1) as f64 / 2.0;
+        let c = y1 as f64;
+        let mul = (num / modulo) as f64;
+
+        f64::ceil(x_2*mul*mul + x_1*mul + c) as usize
+    }
+    fn get_num_plots(&self, max: usize, modulo: bool) -> usize {
         let mut reachable: HashSet<Position> = HashSet::new();
         let mut history: HashSet<(usize,Position)> = HashSet::new();
         let mut queue: VecDeque<(usize, Position)> = VecDeque::new();
@@ -62,7 +87,12 @@ impl Maze {
                 reachable.insert(pos);
             } else if step < max {
                 for curr in [pos.north(), pos.south(), pos.west(), pos.east()].iter() {
-                    if self.check_valid_from_pos(&curr).is_some() && !history.contains(&(step+1,*curr)) {
+                    let next_piece = if modulo {
+                        self.check_valid_from_pos_modulo(&curr)
+                    } else {
+                        self.check_valid_from_pos(&curr)
+                    };
+                    if next_piece.is_some() && !history.contains(&(step+1,*curr)) {
                         queue.push_back((step+1, *curr));
                         history.insert((step+1,*curr));
                     }
@@ -70,6 +100,15 @@ impl Maze {
             }
         }
         reachable.len()
+    }
+    fn check_valid_from_pos_modulo(&self, pos: &Position) -> Option<Pieces> {
+        match self.get_from_pos(&pos.modulo(self.maze[0].len(), self.maze.len())) {
+            Some(x) => match x {
+                Pieces::Rock => None,
+                _ => Some(x),
+            },
+            _ => None,
+        }
     }
     fn check_valid_from_pos(&self, pos: &Position) -> Option<Pieces> {
         match self.get_from_pos(pos) {
@@ -131,11 +170,14 @@ mod tests {
 
     #[test]
     fn test1() {
-        assert_eq!(parse_input("data/test.txt").get_num_plots(6), 16);
+        assert_eq!(parse_input("data/test.txt").get_num_plots(6, false), 16);
     }
 
     #[test]
     fn test2() {
-        assert_eq!(part2("data/test.txt"), 0);
+        assert_eq!(parse_input("data/test.txt").get_num_plots(6, true), 16);
+        assert_eq!(parse_input("data/test.txt").get_num_plots(10, true), 50);
+        assert_eq!(parse_input("data/test.txt").get_num_plots(10, true), 50);
+        assert_eq!(parse_input("data/test.txt").get_num_plots(50, true), 1594);
     }
 }
